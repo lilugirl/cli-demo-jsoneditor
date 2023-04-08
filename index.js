fbc1114f2946f7db7ade19e213a285199d843ed3
yi7.liu <yi7.liu@symbio.com>
2023-04-08 15:31:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+var express = require('express')
+var path = require('path')
+var open = require('open')
+
+var app = express()
+
+// 指定静态资源目录
+app.use(express.static(path.join(__dirname,'vendor/3.3.0_0/')))
+
+// 启动服务器
+app.listen(3026)
+
+open('http://127.0.0.1:3026')
